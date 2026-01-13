0972eab707c4326d8075c6c6c6986ac450b8df30
cleitoncarloss <cleitoncarlos.programmer@gmail.com>
2026-01-13 14:01:17 -0300

    refactor: ajuste de layout

---
+++ src/components/layout/Container.jsx
@@ -0,0 +1,9 @@
+function Container({ children }) {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4">
+      {children}
+    </div>
+  );
+}
+
+export default Container;
